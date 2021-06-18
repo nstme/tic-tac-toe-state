80a3f6644dd47d3b5ee0b5b88d270c2ecdf72284
Aaron Craig <aaron@aaroncraig.com>
2021-06-18 15:13:55 -0700

    add cors module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
